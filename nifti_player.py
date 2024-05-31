86a5c51ca922d13cb74c1a9b3bcb225132f1f27a
eliasmattern <coalcrafter202@gmai.com>
2024-05-31 09:38:50 +0200

    refactor

--- nifti_player.py
+++ nifti_player.py
@@ -16,14 +16,14 @@ for dirpath, dirnames, filenames in os.walk(path):
             print(nii_aff, '\n', nii_hdr)
             print(nii_data.shape)
             print(nii_data.shape[2])
-            if (len(nii_data.shape) == 3):
+            if len(nii_data.shape) == 3:
                 for slice_Number in range(nii_data.shape[2]):
                     plt.imshow(nii_data[:, :, slice_Number])
                     plt.show(block=False)
                     plt.pause(0.04)
                     plt.clf()
 
-            if (len(nii_data.shape) == 4):
+            if len(nii_data.shape) == 4:
                 for frame in range(nii_data.shape[3]):
                     for slice_Number in range(nii_data.shape[2]):
                         plt.imshow(nii_data[:, :, slice_Number, frame])
